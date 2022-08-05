fc43b7f195ca50ed235c849b0f313c350131c52a
Paulo Henrique <paulo.henrique.braguim@outlook.com>
2022-08-05 09:45:49 -0300

    Ajustes no CSS

--- cadastrar.php
+++ cadastrar.php
@@ -9,6 +9,7 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>            
